319cb16c7b9b5f97d3b7757a74e9232f5b3f2524
eslam <eslam.ee600@gmail.com>
2025-05-04 01:15:14 +0300

    refactor

--- src/main/java/com/ecommerce/userservice/model/response/MessageResponse.java
+++ src/main/java/com/ecommerce/userservice/model/response/MessageResponse.java
@@ -1,14 +1,21 @@
 package com.ecommerce.userservice.model.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @Getter
 @Setter
 public class MessageResponse {
+    private int status;
     private String message;
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+    public MessageResponse(int status, String message) {
+        this.message = message;
+        this.status = status;
+    }
 }
